c90f2ad91ab7a128fedf26ec9f8a61c462bed0f5
drrckchng <derrickchungx39@gmail.com>
2022-05-17 17:40:18 +0900

    Add initial loop for 5 round game

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function computerPlay() {
     randomNumber =  Math.floor(Math.random() * 3);
-    if (randomNumber === 0) {
+    if(randomNumber === 0) {
         return "Rock";
-    } else if (randomNumber === 1) {
+    } else if(randomNumber === 1) {
         return "Paper";
     } else {
         return "Scissors";
@@ -12,22 +12,22 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     // validate player selection
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-    if (playerSelection === computerSelection ) {
+    if(playerSelection === computerSelection ) {
         return "Tie! You both chose " + playerSelection;
-    } else if (playerSelection === "Rock") {
+    } else if(playerSelection === "Rock") {
         if (computerSelection === "Paper") {
             return "You lose! Paper beats Rock!";
         } else {
             return "You win! Rock beats Scissors!";
         }
-    } else if (playerSelection === "Paper") {
-        if (computerSelection === "Scissors") {
+    } else if(playerSelection === "Paper") {
+        if(computerSelection === "Scissors") {
             return "You lose! Scissors beats Paper!";
         } else {
             return "You win! Paper beats Rock!";
         }
-    } else if (playerSelection === "Scissors") {
-        if (computerSelection === "Rock") {
+    } else if(playerSelection === "Scissors") {
+        if(computerSelection === "Rock") {
             return "You lose! Rock beats Scissors!";
         } else {
             return "You win! Scissors beats Paper!";
@@ -35,5 +35,13 @@ function playRound(playerSelection, computerSelection) {
     } 
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
+
+// call playRound() to play 5 round game that keeps score and reports winner or loser at the end
+function game() {
+
+    for(let i = 0; i < 5; i++){
+        let userChoice = prompt("Rock, paper, or scissors?");
+        let cpuChoice = computerPlay();
+        console.log(playRound(userChoice, cpuChoice));
+    }
+}
